1e6f1bacf393f3717cbb6e167537188b2bef962a
Matheus Diori <mdiori@hotmail.com>
2022-11-29 22:42:03 -0300

    feat: add 404 error message

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -1,7 +1,18 @@
 import PageLayout from "../../components/page-layout";
 
 const NotFound = () => {
-  return <PageLayout>404</PageLayout>;
+  return (
+    <PageLayout>
+      <div className="flex h-screen">
+        <div className="mx-auto mt-44">
+          <h3 className="text-4xl text-appgreen">
+            Bom não era para você estar aqui, mas já que você chegou toma um 404
+            🥸
+          </h3>
+        </div>
+      </div>
+    </PageLayout>
+  );
 };
 
 export default NotFound;
