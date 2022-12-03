2d5d579e9fafed9c0e13a3e4f7e4510ae1ca3b53
Matheus Diori <mdiori@hotmail.com>
2022-12-03 18:16:54 -0300

    feat: add sweet-alert and react-select
        * create sweet-alert configuration file

---
+++ src/utils/fire-toast.js
@@ -0,0 +1,21 @@
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-right",
+  showConfirmButton: false,
+  timer: 1700,
+  timerProgressBar: true,
+});
+
+export function success(message) {
+  Toast.fire({ icon: "success", title: message });
+}
+
+export function error(message) {
+  Toast.fire({ icon: "error", title: message });
+}
+
+export function warning(message) {
+  Toast.fire({ icon: "warning", title: message });
+}
