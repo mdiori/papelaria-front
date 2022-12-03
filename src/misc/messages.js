a77474807a0893cfed060d9f10a32f37208d2844
Matheus Diori <mdiori@hotmail.com>
2022-12-03 18:16:19 -0300

    feat: add misc and messages file

---
+++ src/misc/messages.js
@@ -0,0 +1,11 @@
+const messages = {
+  form: {
+    product: {
+      select_product_and_quantity: "Selecionar o produto e a quantidade.",
+      add_at_least_one_product: "Adicionar pelo menos um produto.",
+      select_employee_and_client: "Selecionar vendedor e cliente.",
+    },
+  },
+};
+
+export default messages;
